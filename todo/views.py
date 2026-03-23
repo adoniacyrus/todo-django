ec04c05395883c130ee143e67852264768a0ee37
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-03-23 22:55:44 +0530

    Display tasks on homepage

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/index.html', {'tasks': tasks})
