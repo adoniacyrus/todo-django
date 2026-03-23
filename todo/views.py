be8a6ef6190a1e9ccf624f663b6ef8377cda7d5a
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-03-23 23:09:28 +0530

    Added complete and delete functionality

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 def index(request):
     tasks = Task.objects.all()
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+
+        if title:  # validation (no empty tasks)
+            Task.objects.create(title=title)
+
+        return redirect('index')
+    
     return render(request, 'todo/index.html', {'tasks': tasks})
+
+def complete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.completed = not task.completed  # toggle
+    task.save()
+    return redirect('index')
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('index')
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+    path('complete/<int:id>/', views.complete_task, name='complete'),
+    path('delete/<int:id>/', views.delete_task, name='delete'),
 ]
